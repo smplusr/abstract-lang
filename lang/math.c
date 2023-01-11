4f659f56f80d48017286cd424c454baee4327851
smplusr <83000221+smplusr@users.noreply.github.com>
2023-01-11 11:16:53 +0100

    Delete lang directory

--- lang/math.c
+++
@@ -1,7 +0,0 @@
-#include "math.h"
-
-
-size_t mathAdd () { return (double) eval () + (double) eval (); }
-size_t mathSub () { return (double) eval () - (double) eval (); }
-size_t mathMul () { return (double) eval () * (double) eval (); }
-size_t mathDiv () { return (double) eval () / (double) eval (); }
